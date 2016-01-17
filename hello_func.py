f1b5dc550b49ff93b8eb694d92529b3e1fa02dbf
jingri <ri.jing@delphi.com>
2016-01-17 18:47:49 +0800

    modules install and used

---
+++ hello_func.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+' a test module '
+
+__author__ = 'kz9hxb'
+'''
+常用的第三方库:
+MySQL的驱动：mysql-connector-python
+用于科学计算的NumPy库：numpy
+用于生成文本的模板工具:Jinja2
+处理图像的工具库:pillow
+'''
+import sys
+from PIL import Image
+
+def test():
+    args = sys.argv
+    if len(args)==1:
+            print('Hello, world!')
+    elif len(args)==2:
+        print('Hello, %s!' % args[1])
+    else:
+        print('Too many arguments!')
+        
+im = Image.open('test.png')
+print(im.format,im.size,im.mode)
+im.thumbnail((200,100))
+im.save('test_thumb.jpg','JPEG')
+im = Image.open('test_thumb.jpg')
+print(im.format,im.size,im.mode)
+
+if __name__=='__main__':
+    test()
