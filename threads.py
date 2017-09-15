0db1f162e4b9bce2512c21620078ede550af3a0a
jingri <lionel_jing@163.com>
2017-09-15 20:20:54 +0800

    add local thread

--- threads.py
+++ threads.py
@@ -66,7 +66,30 @@ def run_dead_loop():
     for i in range(multiprocessing.cpu_count()):
         t = threading.Thread(target=dead_loop)
         t.start()
+
+#thread local
+
+local_school = threading.local() 
+
+def process_student():
+    std = local_school.student
+    print('Hello, %s (in %s)' % (std, threading.current_thread().name))
+
+def process_thread(name):
+    local_school.student = name
+    process_student()
+
+def use_threadlocal():
+    t1 = threading.Thread(target = process_thread, args = ('Alice',),name = 'Thread-A')
+    t2 = threading.Thread(target = process_thread, args = ('Bob',), name ='Thread-B')
+    t1.start()
+    t2.start()
+    t1.join()
+    t2.join()
+
+
 if __name__ == '__main__':
     #run_thread()
     #run_2_thread()
-    run_dead_loop()
+    #run_dead_loop()
+    use_threadlocal()
