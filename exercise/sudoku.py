b3485a9823d13187ecd5e89c70a807a90ea5bf3b
jingray <lionel_jing@163.com>
2017-10-14 15:27:46 +0800

    add a sudoku exercise

---
+++ exercise/sudoku.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date    : 2017-10-14 09:17:06
+# @Author  : jingray (lionel_jing@163.com)
+# @Link    : http://www.jianshu.com/u/01fb0364467d
+# @Version : $Id$
+
+import os
+# Sudoku [http://en.wikipedia.org/wiki/Sudoku]
+# is a logic puzzle where a game
+# is defined by a partially filled
+# 9 x 9 square of digits where each square
+# contains one of the digits 1,2,3,4,5,6,7,8,9.
+# For this question we will generalize
+# and simplify the game.
+
+# Define a procedure, check_sudoku,
+# that takes as input a square list
+# of lists representing an n x n
+# sudoku puzzle solution and returns the boolean
+# True if the input is a valid
+# sudoku square and returns the boolean False
+# otherwise.
+
+# A valid sudoku square satisfies these
+# two properties:
+
+#   1. Each column of the square contains
+#       each of the whole numbers from 1 to n exactly once.
+
+#   2. Each row of the square contains each
+#       of the whole numbers from 1 to n exactly once.
+
+# You may assume the the input is square and contains at
+# least one row and column.
+
+correct = [[1,2,3],
+           [2,3,1],
+           [3,1,2]]
+
+incorrect = [[1,2,3,4],
+             [2,3,1,3],
+             [3,1,2,3],
+             [4,4,4,4]]
+
+incorrect2 = [[1,2,3,4],
+             [2,3,1,4],
+             [4,1,2,3],
+             [3,4,1,2]]
+
+incorrect3 = [[1,2,3,4,5],
+              [2,3,1,5,6],
+              [4,5,2,1,3],
+              [3,4,5,2,1],
+              [5,6,4,3,2]]
+
+incorrect4 = [['a','b','c'],
+              ['b','c','a'],
+              ['c','a','b']]
+
+incorrect5 = [ [1, 1.5],
+               [1.5, 1]]
+
+def check_sudoku(square_list):
+    length = len(square_list)
+    all_list = square_list
+    row_list = []
+    result = True
+    for j in range(0,length):
+        for i in range(0,length):
+            row_list.append(square_list[i][j])
+        all_list.append(row_list)
+        row_list = []
+    #print all_list
+    for colume in all_list:
+        for i in range(1,length+1):
+            if i not in colume:
+                result = False
+    return result
+
+
+
+
+
+
+#print check_sudoku(incorrect)
+#>>> False
+
+#print check_sudoku(correct)
+#>>> True
+
+#print check_sudoku(incorrect2)
+#>>> False
+
+#print check_sudoku(incorrect3)
+#>>> False
+
+#print check_sudoku(incorrect4)
+#>>> False
+
+#print check_sudoku(incorrect5)
+#>>> False
+
+
